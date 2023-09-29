42ef639b3b932c9a195c86b4f9f3514f8c894adb
First Fractal <firstfractal512@gmail.com>
2023-09-29 12:46:13 -0500

    added in a config option for allowing the nurse to heal

--- NoNaturalRegen.cs
+++ NoNaturalRegen.cs
@@ -3,6 +3,8 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using System.ComponentModel;
 
 namespace NoNaturalRegen
 {
@@ -22,29 +24,39 @@ namespace NoNaturalRegen
         //this function will make it that the nurse will insta-kill the player, if set to true.
         public override bool ModifyNurseHeal(NPC nurse, ref int health, ref bool removeDebuffs, ref string chatText)
         {
-            //no one should surivie the nurse
-            Player.immune = false;
-            Player.immuneTime = 0;
-            Player.creativeGodMode = false;
-            Player.onHitDodge = false;
-            Player.shadowDodge = false;
-
-            //get the name of the nurse
-            string Nurse = " ";
-            foreach (NPC n in Main.npc)
+            //check if the player doesn't allow the  player to heal with the nurse
+            if (!NNRConfig.Instance.allowNurseHealing)
             {
-                if (n.type == NPCID.Nurse)
-                {
-                    Nurse = n.FullName;
-                }
-            }
-            //pick a random death message for dying to the nurse
-            string deathMessageType = "Mods.NoNaturalRegen.DeathMessage.Nurse" + Main.rand.Next(0, 7).ToString();
+                //no one should surivie the nurse
+                Player.immune = false;
+                Player.immuneTime = 0;
+                Player.creativeGodMode = false;
+                Player.onHitDodge = false;
+                Player.shadowDodge = false;
 
-            //insta-kill the player
-            Player.Hurt(PlayerDeathReason.ByCustomReason(Language.GetTextValue(deathMessageType, Player.name, Nurse)), 999999, 1, dodgeable: false, armorPenetration: 9999);
+                //pick a random death message for dying to the nurse
+                string deathMessageType = "Mods.NoNaturalRegen.DeathMessage.Nurse" + Main.rand.Next(0, 7).ToString();
+
+                //insta-kill the player
+                Player.Hurt(PlayerDeathReason.ByCustomReason(Language.GetTextValue(deathMessageType, Player.name, nurse.FullName)), 999999, 1, dodgeable: false, armorPenetration: 9999);
+                return false;
+            }
+            
             return base.ModifyNurseHeal(nurse, ref health, ref removeDebuffs, ref chatText);
         }
+    }
+
+    public class NNRConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+        public static NNRConfig Instance;
+
+        [Header("GeneralOptions")]
+
+        //[Label("$Mods.NoNaturalRegen.Configs.allowNurseHealing.Label")]
+        //[Tooltip("$Mods.NoNaturalRegen.Configs.allowNurseHealing.Tooltip")]
+        [DefaultValue(false)]
+        public bool allowNurseHealing;
     }
 }
 
