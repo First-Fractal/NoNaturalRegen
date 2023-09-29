5991a588dc36205f38b9f699c43efc5d13816246
First Fractal <firstfractal512@gmail.com>
2023-09-29 12:24:43 -0500

    Made the player die to the nurse when trying to heal with her

--- NoNaturalRegen.cs
+++ NoNaturalRegen.cs
@@ -1,4 +1,7 @@
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace NoNaturalRegen
@@ -14,6 +17,33 @@ namespace NoNaturalRegen
         {
             Player.lifeRegenTime = 0;
             base.UpdateLifeRegen();
+        }
+
+        //this function will make it that the nurse will insta-kill the player, if set to true.
+        public override bool ModifyNurseHeal(NPC nurse, ref int health, ref bool removeDebuffs, ref string chatText)
+        {
+            //no one should surivie the nurse
+            Player.immune = false;
+            Player.immuneTime = 0;
+            Player.creativeGodMode = false;
+            Player.onHitDodge = false;
+            Player.shadowDodge = false;
+
+            //get the name of the nurse
+            string Nurse = " ";
+            foreach (NPC n in Main.npc)
+            {
+                if (n.type == NPCID.Nurse)
+                {
+                    Nurse = n.FullName;
+                }
+            }
+            //pick a random death message for dying to the nurse
+            string deathMessageType = "Mods.NoNaturalRegen.DeathMessage.Nurse" + Main.rand.Next(0, 7).ToString();
+
+            //insta-kill the player
+            Player.Hurt(PlayerDeathReason.ByCustomReason(Language.GetTextValue(deathMessageType, Player.name, Nurse)), 999999, 1, dodgeable: false, armorPenetration: 9999);
+            return base.ModifyNurseHeal(nurse, ref health, ref removeDebuffs, ref chatText);
         }
     }
 }
