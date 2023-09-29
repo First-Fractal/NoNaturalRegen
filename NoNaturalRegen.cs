cdb2b7344b53cdc570db6bd326d367ea6a7b93d0
First Fractal <firstfractal512@gmail.com>
2023-09-29 12:21:02 -0500

    Made the player unable to naturally regen

--- NoNaturalRegen.cs
+++ NoNaturalRegen.cs
@@ -5,7 +5,16 @@ namespace NoNaturalRegen
 {
 	public class NoNaturalRegen : Mod
 	{
-
 	}
+
+	public class NNRPlayer : ModPlayer
+	{
+        //this function makes the natural regen set to zero. regen accessories will still act normally.
+        public override void UpdateLifeRegen()
+        {
+            Player.lifeRegenTime = 0;
+            base.UpdateLifeRegen();
+        }
+    }
 }
 
